a4240c83e2452b3d193e8bc7573b2247f526f7f2
naka848 <n.moeko1966@gmail.com>
2021-12-15 14:22:00 +0900

    変更

---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ data.title }}
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "HelloWorld",
+  setup() {
+    const data = reactive({
+      title: "Vueeeee",
+    });
+    return {
+      data,
+    };
+  },
+};
+</script>
