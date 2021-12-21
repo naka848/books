b3f1045aeb4e7ccb718d5bff590e6c7b15199e55
nigaya <n.moeko1966@gmail.com>
2021-12-21 21:00:29 +0900

    悩み中

--- resources/js/components/Find.vue
+++ resources/js/components/Find.vue
@@ -1,29 +1,30 @@
 <template>
     <div>
         <h3>書籍検索</h3>
-        <input type="text"  v-model="data.find">
-        <button @click="getData">検索</button>
+        <form>
+            <input type="text"  v-model="data.find">
+            <button @click="getData">検索</button>
+        </form>
     </div>
 </template>
 
 <script>
-// import { reactive } from "vue";
-// import axios from "axios";
+import { reactive } from "vue";
+import axios from "axios";
 
 // let url = "http://127.0.0.1:8000/find"
 
 export default{
     name: "Find",
-    // setup() {
-    //     const data = reactive({
-    //         find: '',
-    //     });    
-    // },
-    // まずはＬＩＳＴにアクセスできるか確認
-    // const getData = ()=> {
-    //     axios.get("http://127.0.0.1:8000/list");
+    setup() {
+        const data = reactive({
+            find: '',
+        });    
+    },
 
-    // },
+    const getData(){
+        axios.post("/list",data.find)
+    }
 }
 </script>
 
