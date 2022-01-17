f77f29aba4595aa960e078bd71d243e25aee6b2a
nigaya <n.moeko1966@gmail.com>
2022-01-17 21:48:26 +0900

    一旦保管

--- resources/js/components/Find2.vue
+++ resources/js/components/Find2.vue
@@ -3,19 +3,28 @@
     <h3>書籍検索</h3>
     <input type="text" v-model="data.find" />
     <button @click="getData">検索</button>
+    <Find2C
+      v-for="data in data.book_list"
+      v-bind:book_list="data"
+      v-bind:key="data.book_id"
+    />
   </div>
 </template>
 
 <script>
 import { reactive } from "vue";
 import axios from "axios";
+import Find2C from "./Find2C";
 
 export default {
   name: "Find2",
+  components: {
+    Find2C,
+  },
   setup() {
     const data = reactive({
       find: "vue",
-      book: [],
+      book_list: [],
     });
 
     const getData = async () => {
@@ -24,7 +33,20 @@ export default {
         "http://127.0.0.1:8000/api/book_information" + querry
       );
 
-      console.log(result.data);
+      // console.log(result.data);
+
+      // 要素1個しかだせなかった
+      // result.data.forEach(function(element){
+      //   data.book = element;
+      // });
+
+      result.data.map((book_lists) => {
+        book_lists.map((book_list) => {
+          data.book_list = book_list;
+        });
+      });
+
+      console.log(data.book_list);
 
 
     };
---
+++ resources/js/components/Find2C.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ book_list }}
+    <!-- 「{{ book_list.title }}」
+    {{ book_list.author }} 著
+    出版 : {{ book_list.publisher }} ({{ book_list.published }}) -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Find2C",
+  props:{
+    book_list: Object,
+  },
+};
+</script>
